39fb6c02bd7435044e286a1839246f9acd554600
Branimir Djordjevic <branimir1111@gmail.com>
2023-10-03 14:22:34 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  BrowserRouter,
+} from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <About />,
+//   },
+// ]);
+
+const App = () => {
+  // return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
